b40e9363040212643eae317b86f29f0ceec1cce3
Tommaso Bossi <tommasobossi95+github@gmail.com>
2018-02-05 18:28:52 +0100

    Target mode improved

--- Shootball/Assets/Scripts/Model/Robot/PlayerRobotModel.cs
+++ Shootball/Assets/Scripts/Model/Robot/PlayerRobotModel.cs
@@ -5,24 +5,39 @@ namespace Shootball.Model.Robot
 {
     public class PlayerRobotModel : RobotModel
     {
+        private readonly float _maxTargetFieldOfView;
+        private float _currentFieldOfView;
+
         public PlayerRobotModel(RobotSettings settings, RobotComponents components, RobotStatistics statistics,
-				Color laserColor) : base(settings, components, statistics, Color.blue, laserColor)
+                Color laserColor) : base(settings, components, statistics, Color.blue, laserColor)
         {
-			Components.RobotHead.AddComponent<AudioListener>();
+            Components.RobotHead.AddComponent<AudioListener>();
+            _maxTargetFieldOfView = Components.TargetCamera.GetComponent<Camera>().fieldOfView;
+            _currentFieldOfView = Components.HeadCamera.GetComponent<Camera>().fieldOfView;
         }
 
-		public void Target(bool useTarget)
-		{
-			if (useTarget)
-			{
-				Components.HeadCamera.GetComponent<Camera>().enabled = false;
-				Components.TargetCamera.GetComponent<Camera>().enabled = true;
-			}
-			else
-			{
-				Components.TargetCamera.GetComponent<Camera>().enabled = false;
-				Components.HeadCamera.GetComponent<Camera>().enabled = true;
-			}
-		}
+        public void Target(bool useTarget)
+        {
+            if (useTarget)
+            {
+                _currentFieldOfView = Mathf.Lerp(_maxTargetFieldOfView, _currentFieldOfView, 0.5f);
+                Components.HeadCamera.GetComponent<Camera>().enabled = false;
+                Components.TargetCamera.GetComponent<Camera>().enabled = true;
+            }
+            else
+            {
+                var minFieldOfView = Components.HeadCamera.GetComponent<Camera>().fieldOfView;
+				_currentFieldOfView = Mathf.Lerp(minFieldOfView, _currentFieldOfView, 0.4f);
+				
+                if (Mathf.Abs(_currentFieldOfView - minFieldOfView) < 5)
+                {
+                    _currentFieldOfView = minFieldOfView;
+                    Components.TargetCamera.GetComponent<Camera>().enabled = false;
+                    Components.HeadCamera.GetComponent<Camera>().enabled = true;
+                }
+            }
+            
+			Components.TargetCamera.GetComponent<Camera>().fieldOfView = _currentFieldOfView;			
+        }
     }
 }
